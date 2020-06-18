576670c840c265c3eee361853aea53ae268c991c
Florian Hansmann <florianhansmann@FloriansMacBook.fritz.box>
2020-06-18 20:59:01 +0200

    furhter graphql-ing

---
+++ src/templates/blog.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+
+const Blog = () => {
+    return(
+        <Layout>
+            <p>
+                This is the blog template
+            </p>
+        </Layout>
+    )
+}
+
+export default Blog
