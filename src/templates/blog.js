3b79c4d7231d79ff49a1a68f00854d7debbf53e8
Florian Hansmann <florianhansmann@FloriansMacBook.fritz.box>
2020-06-18 21:35:14 +0200

    data update

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,13 +1,32 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { graphql } from 'gatsby'
+
+export const query = graphql`
+    query ($slug: String)
+    {
+        markdownRemark ( 
+        fields: {
+        slug:{
+            eq: $slug
+        }
+        }
+    ) {
+        frontmatter {
+        title
+        date
+        }
+        html
+    }
+    }`
 
 
-const Blog = () => {
+const Blog = (props) => {
     return(
         <Layout>
-            <p>
-                This is the blog template
-            </p>
+            <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+            <p>{props.data.markdownRemark.frontmatter.date}</p>
+            <div dangerouslySetInnerHTML = {{ __html: props.data.markdownRemark.html}}></div>
         </Layout>
     )
 }
