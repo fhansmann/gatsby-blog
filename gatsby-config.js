2f677f9eecbc6b69d304d6926c0b48a55c179eb8
Florian Hansmann <florianhansmann@FloriansMacBook.fritz.box>
2020-06-18 21:52:01 +0200

    there is an error somwhere

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,8 +16,24 @@ module.exports = {
         path: `${__dirname}/src/`
       }
     },
-    'gatsby-transformer-remark'
+    
+    'gatsby-transformer-remark',
 
+    'gatsby-plugin-sharp',
+
+    {
+      resolve: 'gatsby-transformer-remark',
+      
+      options: {
+        plugins: [
+            'gatsby-remark-relative-images',
+            {
+              resolve: 'gatsby-remark-images',
+              options: {
+                maxWidth: 750,
+                linkImagesToOriginal: false
+              }
+            }
+        ]
+      }
   ]
-
-}
--- src/posts/gatsby.md
+++ src/posts/gatsby.md
@@ -3,6 +3,8 @@ title: "Wow"
 date: "2019 - Juli - 1"
 ---
 
+![Grass](./Grass.jpg)
+
 I just launched a new bootcamp!
 
 ## Topics Covered
